db93d0f149bf06110ca0ed200d535c6d91ef9fee
Xynon <63993670+Xynonners@users.noreply.github.com>
2023-08-16 04:52:08 -0700

    finish previous commit

--- src/fletbox/fletbox.py
+++ src/fletbox/fletbox.py
@@ -48,7 +48,10 @@ class Builder():
             if inspect.isclass(cls):
                 if issubclass(cls, ft.Control):
                     all_elements.append(cls)
-                    if len([*set(dir(cls)).intersection(["controls", "actions", "content"])]) >= 1:
+                    matches = len([*set(dir(cls)).intersection(["controls", "actions", "content", "tabs", "title"])])
+                    if matches > 1:
+                        print(f"[bold red]{name} matched {matches} possible subcontrol kwargs.[/bold red]")
+                    elif matches == 1:
                         filtered_elements.append(cls)
         return all_elements, filtered_elements
 
